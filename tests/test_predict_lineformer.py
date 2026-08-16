1b514ba2853e78ae65cfdd431059177a41808a3d
akabynda <danilguy7@gmail.com>
2026-08-17 01:40:30 +0300

    remove unfinished LineFormer runner

--- tests/test_predict_lineformer.py
+++
@@ -1,6 +0,0 @@
-from training.predict_lineformer import PALETTE
-
-
-def test_lineformer_visualization_palette_is_nonempty() -> None:
-    assert len(PALETTE) >= 8
-    assert all(len(color) == 3 for color in PALETTE)
